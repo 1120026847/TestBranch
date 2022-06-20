5beec293ccbd91128c8235231ac777acd93201c4
陌离 <5629141+molidata@user.noreply.gitee.com>
2022-06-20 11:26:20 +0800

    init

---
+++ app/src/main/java/com/example/testbranch/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testbranch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
+    }
+}
